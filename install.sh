adbe7c350a0086dc7bbd3578ac4c14e37f7f0e23
Mohit Swain <swain6564@gmail.com>
2021-02-14 19:52:24 +0530

    Final edit

--- install.sh
+++ install.sh
@@ -16,10 +16,7 @@ sudo apt-get update
 
 # Install the latest version of PostgreSQL.
 # If you want a specific version, use 'postgresql-12' or similar instead of 'postgresql':
- sudo apt-get -y install postgresql
+sudo apt-get -y install postgresql
 
-
-sudo su - postgres 
-
-psql -c "CREATE DATABASE memedb;"
-
+sudo -u postgres psql -c "CREATE DATABASE memedb;"
+sudo -u postgres psql -c "ALTER USER postgres PASSWORD '1234';"
--- server_run.sh
+++ server_run.sh
@@ -5,7 +5,7 @@ rm .env
 touch .env
 
 echo -e "IMGUR_CLIENT_ID=e3c357d5aa7c35d" >> .env
-echo -e "DB_PASSWORD=null" >> .env
+echo -e "DB_PASSWORD=1234" >> .env
 
 
 npm install
